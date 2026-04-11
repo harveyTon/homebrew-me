3464d7bb2940a7ec548687afa797f7700b185fd7
Harvey <119395663+harveyTon@users.noreply.github.com>
2026-04-11 23:04:07 +0800

    Update me formula to v0.2.0

--- Formula/me.rb
+++ Formula/me.rb
@@ -1,8 +1,8 @@
 class Me < Formula
   desc "A modern, context-aware replacement for whoami"
   homepage "https://github.com/harveyTon/me"
-  url "https://github.com/harveyTon/me/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "fe63f5e6fa5e866a42a2c966e5fde142ac36bc8f39aeb37de8e1406527f64594"
+  url "https://github.com/harveyTon/me/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "e07b207141dbcdb1da03590f29c65117477452399206892a191cb53e09d14e47"
   license "MIT"
 
   depends_on "rust" => :build
